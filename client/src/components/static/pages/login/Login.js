43c7ea18e4ef75b5b62b7505819bd7a0f855327d
Ibad Ali <ibad.ali@arhamsoft.org>
2022-03-29 15:04:37 +0500

    account pages desgin change

--- client/src/components/static/pages/login/Login.js
+++ client/src/components/static/pages/login/Login.js
@@ -4,6 +4,7 @@ import { ToastContainer, toast } from "react-toastify";
 // import auth from "../../services/auth.service";
 // import { ENV } from "../../env";
 import $ from "jquery";
+import Logo from "../../common/logo/Logo";
 
 const Login = () => {
   // Initial Values
@@ -52,88 +53,79 @@ const Login = () => {
 
   return (
     <div className="account-page">
-        <div className="content">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-8 offset-md-2">
-              <div className="account-content">
-                <div className="row align-items-center justify-content-center">
-                  <div className="col-md-7 col-lg-6 login-left">
-                    <img
-                      src="assets/img/login-banner.png"
-                      className="img-fluid"
-                      alt="Login"
+      <div className="content">
+        <div className="text-center mb-md-5 mb-3">
+          <Logo />
+        </div>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-5 col-md-7 text-center ">
+              <div className="account-content login-right">
+                <div className="login-header">
+                  <ToastContainer
+                    position="top-center"
+                    autoClose={300}
+                    hideProgressBar
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    theme="colored"
+                  />
+                  <h3>Login</h3>
+                </div>
+                <form action="#">
+                  <div className="form-group form-focus">
+                    <input
+                      type="email"
+                      name="email"
+                      value={loginUser.email}
+                      onChange={handleChange}
+                      className="form-control floating"
                     />
+                    <label className="focus-label">Email</label>
                   </div>
-                  <div className="col-md-12 col-lg-6 login-right">
-                    <div className="login-header">
-                      <ToastContainer
-                        position="top-center"
-                        autoClose={300}
-                        hideProgressBar
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        theme="colored"
-                      />
-                      <h3>Login</h3>
-                    </div>
-                    <form action="#">
-                      <div className="form-group form-focus">
-                        <input
-                          type="email"
-                          name="email"
-                          value={loginUser.email}
-                          onChange={handleChange}
-                          className="form-control floating"
-                        />
-                        <label className="focus-label">Email</label>
-                      </div>
-                      <div className="form-group form-focus">
-                        <input
-                          type="password"
-                          name="password"
-                          value={loginUser.password}
-                          onChange={handleChange}
-                          className="form-control floating"
-                        />
-                        <label className="focus-label">Password</label>
-                      </div>
-                      <div className="text-right">
-                        <Link className="forgot-link" to="/forgot-password">
-                          Forgot Password ?
-                        </Link>
-                      </div>
-                      <button
-                        className="btn btn-primary btn-block btn-lg login-btn"
-                        type="button"
-                        onClick={LoginCall}
-                      >
-                        Login
-                      </button>
-                      <div className="login-or">
-                        <span className="or-line"></span>
-                        <span className="span-or">or</span>
-                      </div>
-                      <div className="row form-row social-login">
-                        {/* <div className="col-6">
+                  <div className="form-group form-focus">
+                    <input
+                      type="password"
+                      name="password"
+                      value={loginUser.password}
+                      onChange={handleChange}
+                      className="form-control floating"
+                    />
+                    <label className="focus-label">Password</label>
+                  </div>
+                  <div className="text-right">
+                    <Link className="forgot-link" to="/forgot-password">
+                      Forgot Password ?
+                    </Link>
+                  </div>
+                  <button
+                    className="btn btn-primary btn-block btn-lg login-btn"
+                    type="button"
+                    onClick={LoginCall}
+                  >
+                    Login
+                  </button>
+                  <div className="login-or">
+                    <span className="or-line"></span>
+                    <span className="span-or">or</span>
+                  </div>
+                  <div className="row form-row social-login">
+                    {/* <div className="col-6">
                           <a href="#" className="btn btn-facebook btn-block">
                             <i className="fab fa-facebook-f mr-1"></i> Login
                           </a>
                         </div> */}
-                        <div className="col-12">
-                          <a href="#" className="btn btn-google btn-block">
-                            <i className="fab fa-google mr-1"></i> Login
-                          </a>
-                        </div>
-                      </div>
-                      <div className="text-center dont-have">
-                        Don’t have an account?{" "}
-                        <Link to="/signup">Register</Link>
-                      </div>
-                    </form>
+                    <div className="col-12">
+                      <a href="#" className="btn btn-google btn-block">
+                        <i className="fab fa-google mr-1"></i> Login
+                      </a>
+                    </div>
                   </div>
-                </div>
+                  <div className="text-center dont-have">
+                    Don’t have an account? <Link to="/signup">Register</Link>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
