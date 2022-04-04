94411b072b44bd757bd5d15fd44f0cc48c2d6309
Omer Shahzad <omershahzad707@gmail.com>
2022-04-04 20:05:56 +0500

    update profile picture

--- client/src/components/app/pages/ClientLongevityClub/ClientLongevityClub.js
+++ client/src/components/app/pages/ClientLongevityClub/ClientLongevityClub.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 const ClientLongevityClub = () => {
   return (
     <>
-    <h1>Coaches may stake at least 25% of their tokens for 12-Months into the liquidity pool to receive the
-following benefits: (This is the recommended Vesting Period To Avoid Manipulation in price)</h1>
-    <h3>1% Wealth Club bonus token rewarded</h3>
-    <h3>1% bonus token</h3>
+      <h1>
+        Coaches may stake at least 25% of their tokens for 12-Months into the
+        liquidity pool to receive the following benefits: (This is the
+        recommended Vesting Period To Avoid Manipulation in price)
+      </h1>
+      <h3>1% Wealth Club bonus token rewarded</h3>
+      <h3>1% bonus token</h3>
       <p>a. For 4+ star client rating</p>
-    <h3>50% reduced platform transaction fee</h3>
-    <h3>50% reduced Withdrawal Funds conversion fee</h3>
-    <h3>50% reduced Swap Coin conversion fee</h3>
-    <h3>100,000 RXHEAL token to be redeemed for Lifestyle Prescriptions® University Master’s or Ph.D.</h3>
-    <h3>Degree Programs</h3>
-    
-        </>
-  )
-}
-export default ClientLongevityClub
+      <h3>50% reduced platform transaction fee</h3>
+      <h3>50% reduced Withdrawal Funds conversion fee</h3>
+      <h3>50% reduced Swap Coin conversion fee</h3>
+      <h3>
+        100,000 RXHEAL token to be redeemed for Lifestyle Prescriptions®
+        University Master’s or Ph.D.
+      </h3>
+      <h3>Degree Programs</h3>
+    </>
+  );
+};
+export default ClientLongevityClub;
