1d08da297e4b6fc8ab11ea7fb38548705b184bd4
Usman Munir <musman.muneer@arhamsoft.org>
2022-04-05 15:02:44 +0500

    set coach profile setting

--- client/src/components/app/pages/CoachProfile/CoachProfile.js
+++ client/src/components/app/pages/CoachProfile/CoachProfile.js
@@ -26,30 +26,7 @@ const CoachProfile = () => {
   }, []);
   return (
     <>
-      {/* // breadcrumb */}
-      <div className="breadcrumb-bar">
-        <div className="container-fluid">
-          <div className="row align-items-center">
-            <div className="col-md-12 col-12">
-              <nav aria-label="breadcrumb" className="page-breadcrumb">
-                <ol className="breadcrumb">
-                  <li className="breadcrumb-item">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li className="breadcrumb-item active" aria-current="page">
-                    Coach Profile
-                  </li>
-                </ol>
-              </nav>
-              <h2 className="breadcrumb-title">Coach Profile</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* // breadcrumb end */}
-      <div className="content">
-        <div className="container">
-          {/* <!-- Doctor Widget --> */}
+      <div className="col-md-7 col-lg-8 col-xl-9">
           <div className="card">
             <div className="card-body">
               <div className="doctor-widget">
@@ -1058,8 +1035,7 @@ const CoachProfile = () => {
             </div>
           </div>
           {/* <!-- /Doctor Details Tab --> */}
-        </div>
-      </div>
+          </div>
     </>
   );
 };
