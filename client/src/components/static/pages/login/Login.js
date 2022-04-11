acab81f45ad954e07af6b0530e988369e48eeb2a
Ibad Ali <ibad.ali@arhamsoft.org>
2022-04-11 14:12:27 +0500

    googlelogin

--- client/src/components/static/pages/login/Login.js
+++ client/src/components/static/pages/login/Login.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Spinner } from "react-bootstrap";
+import { Modal, Spinner, Button } from "react-bootstrap";
 import Toast from "../../../common/toast/Toast";
 import { setInfoData } from "../../../../store/slices/user";
 import { Link, useHistory, useLocation } from "react-router-dom";
@@ -14,6 +14,7 @@ import {
   cancelOngoingHttpRequest,
   getHttpRequest,
   postHttpRequest,
+  putHttpRequest,
 } from "../../../../axios";
 import {
   setUser,
@@ -29,18 +30,26 @@ const Login = (props) => {
   const dispatch = useDispatch();
   const emailRef = useRef();
   const passwordRef = useRef();
+  const typeRef = useRef();
 
   const [validationErrors, setValidationErrors] = useState({});
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
+  const [show, setShow] = useState(false);
+  const [_id, setId] = useState("");
   const handleLogin = (response) => {
-    console.log(response.tokenId);
+    console.log(response);
 
     postHttpRequest("/front/auth/googleLogin", {
       tokenId: response.tokenId,
     }).then((response) => {
       console.log("google login success", response);
+      if (response.status === 200) {
+        console.log("alert", response.data.user._id);
+        dispatch(setAccessToken(response.data.accessToken));
+        setId(response.data.user._id);
+        setShow(true);
+      }
     });
   };
 
@@ -124,8 +133,189 @@ const Login = (props) => {
     }
   };
 
+  const loginGmailHanlder = async (event) => {
+    event.preventDefault();
+
+    const type = typeRef.current.value;
+
+    const payload = {
+      _id,
+      type,
+    };
+
+    const errors = validate(payload);
+
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors({ ...errors });
+      return;
+    } else {
+      setValidationErrors({});
+    }
+    setIsLoading(true);
+
+    try {
+      let response = await putHttpRequest("/front/auth/role", payload);
+      console.log(response);
+      if (!response) {
+        Toast.fire({
+          icon: "error",
+          title: response.data.message,
+        });
+        return;
+      }
+
+      if (response.data.coach.status === true) {
+        setIsLoading(false);
+        let res = await getHttpRequest(
+          `/front/coach/get/${response?.data?.coach?._id}`
+        );
+        console.log("res ", res);
+        if (res) {
+          const userData = {
+            role: response?.data?.data?.type,
+            name: response?.data?.data?.name,
+            email: response?.data?.data?.email,
+            _id: response?.data?.data?._id,
+            about: res?.data.coach.about,
+            firstname: res?.data.coach.firstname,
+            lastname: res?.data.coach.lastname,
+            specialization: res?.data.coach.specialization,
+            profile: res?.data.coach.profile,
+          };
+          dispatch(setUser(userData));
+        debugger;
+
+          if (response?.data?.coach?.type == 1) {
+            history.replace(CLIENT_DASHBOARD);
+          } else if (response?.data?.coach?.type == 3) {
+            history.replace(COACH_DASHBOARD);
+          }
+        } else {
+          Toast.fire({
+            icon: "error",
+            title: response.data.message,
+          });
+          return;
+        }
+      }
+    } catch (e) {
+      setIsLoading(false);
+      Toast.fire({
+        icon: "error",
+        title: "Something went wrong",
+      });
+    }
+
+    //     putHttpRequest("/front/auth/role", payload).then((response) => {
+    //       setIsLoading(false);
+
+    //       if (!response) {
+    //         console.log("Something went wrong with response...");
+    //         return;
+    //       }
+    // console.log("Rssssssssssssssssssss", response)
+
+    //       if (response.data.coach.status === true) {
+    //         setValidationErrors({});
+
+    //         fetch(
+    //           `/front/coach/get/${response?.data?.coach?._id}`
+    //         ).then(resp => resp.json()).then(data => {
+    //           console.log("asdjkashdkhasjkdhajksdhajkhdjkashdjkahsdjkahsdjkh",data);
+    //     })
+
+    //         // if (response) {
+    //         //   setIsLoading(false);
+
+    //         //   if (res) {
+    //         //     const userData = {
+    //         //       role: response?.data?.data?.type,
+    //         //       name: response?.data?.data?.name,
+    //         //       email: response?.data?.data?.email,
+    //         //       _id: response?.data?.data?._id,
+    //         //       about: res?.data.coach.about,
+    //         //       firstname: res?.data.coach.firstname,
+    //         //       lastname: res?.data.coach.lastname,
+    //         //       specialization: res?.data.coach.specialization,
+    //         //       profile: res?.data.coach.profile,
+    //         //     };
+    //         //     dispatch(setUser(userData));
+    //         //     dispatch(setAccessToken(response.data.data.accessToken));
+
+    //         //     if (response?.data?.data?.type == 1) {
+    //         //       history.replace(CLIENT_DASHBOARD);
+    //         //     } else if (response?.data?.data?.type == 3) {
+    //         //       history.replace(COACH_DASHBOARD);
+    //         //     }
+    //         //   } else {
+    //         //     Toast.fire({
+    //         //       icon: "error",
+    //         //       title: response.data.message,
+    //         //     });
+    //         //     return;
+    //         //   }
+    //         // }
+    //         Toast.fire({
+    //           customClass: {
+    //             denyButton: "deny-class",
+    //             confirmButton: "confirm-class",
+    //             title: "title",
+    //             htmlContainer: "text",
+    //           },
+    //           icon: "success",
+    //           width: "450px",
+    //           padding: "20px 10px",
+    //           title: `Registered Successfully!`,
+    //           confirmButtonText: `OK`,
+    //           text: `${response.data.message}`,
+    //         })
+    //         .then(() => {
+
+    //           console.log("fonal", response)
+    //           if (response?.data?.coach?.type == 1) {
+    //             history.replace(CLIENT_DASHBOARD);
+    //           } else if (response?.data?.coach?.type == 3) {
+    //             history.replace(COACH_DASHBOARD);
+    //           }
+    //           console.log("final user type", response.data.coach.type)
+    //         });
+    //       } else {
+    //         setValidationErrors(response.data.errorObj);
+
+    //         Toast.fire({
+    //           icon: "error",
+    //           title: response.data.message,
+    //         });
+    //       }
+    //     });
+  };
+
   return (
     <div className="account-page">
+      <Modal show={show}>
+        <Modal.Header>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Kindly select your role
+          <div className="form-floating mb-4">
+            <select className="form-select" ref={typeRef}>
+              <option value="" selected disabled>
+                Open this select menu
+              </option>
+              <option name="3" value="3">
+                Coach
+              </option>
+              <option name="1" value="1">
+                Client
+              </option>
+            </select>
+            <label>Register as a</label>
+            <span className="errors">{validationErrors?.type}</span>
+          </div>
+          <Button onClick={loginGmailHanlder}></Button>
+        </Modal.Body>
+      </Modal>
       <div className="content">
         <div className="text-center mb-md-5 mb-3">{/* <Logo /> */}</div>
         <div className="container">
